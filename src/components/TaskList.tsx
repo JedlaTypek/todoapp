c8ac7136ddba4ee76df9c2668989d8b1692523d9
unknown <jedlicka.filip@seznam.cz>
2024-09-09 22:16:02 +0200

    Přidán počet úkolů

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -10,20 +10,23 @@ interface Props{
 }
 
 function TaskList({heading, done, tasks, onChangeDone, onRemove}: Props){
+    let count = 0;
+    tasks.forEach((task) => task.done == done ? count++ : null);
     const sortedTasks = tasks.sort((a, b) => {
         // Pokud oba úkoly nemají deadline, považujeme je za rovné
-        if (a.deadline == '' && b.deadline == '') return 0;
+        if (a.deadline == "" && b.deadline == "") return 0;
         
         // Pokud jeden úkol nemá deadline, bude na konci
-        if (a.deadline == '') return 1;
-        if (b.deadline == '') return -1;
+        if (a.deadline == "") return 1;
+        if (b.deadline == "") return -1;
         
-        // Porovnání deadlinů, pokud oba úkoly mají deadline
-        return (a.deadline.getTime() - b.deadline.getTime());
+        const aDeadlineDate = new Date(a.deadline);
+        const bDeadlineDate = new Date(b.deadline);
+        return (aDeadlineDate.getTime() - bDeadlineDate.getTime());
       });
     return(
         <>
-            <h2>{heading}</h2>
+            <h2>{heading + ' (' + count + ')'}</h2>
             <div className={'tasklist' + (done ? ' done' : '')}>
                 {sortedTasks.length === 0 ? 'Žádné úkoly.' : sortedTasks.map((task, index) => (task.done == done ? <TaskItem onRemove={onRemove} onChangeDone={onChangeDone} key={index} index={index} name={task.name} deadline={task.deadline} done={done}/> : null))}
             </div>
